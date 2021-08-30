9418f7f73806441eb25ec24d435e50b8ff1e07c3
Alec Kienzle <alec.kienzle@gmail.com>
2021-08-30 15:50:13 -0700

    fixed bug in freeTree()

--- BinarySearchTree/main.cpp
+++ BinarySearchTree/main.cpp
@@ -45,9 +45,23 @@ public:
             
     }
     
+    string getMin2() const
+    {
+        Node *current = root;
+        
+        if (current == nullptr)
+            return("null");
+        
+        while (current->left != nullptr)
+        {
+            current = current->left;
+        }
+        return(current->value);
+    }
+    
     string getMax(Node* rootPtr) const
     {
-        if (root == nullptr)
+        if (rootPtr == nullptr)
             return("Null");
         
         if(rootPtr->right == nullptr)
@@ -56,7 +70,54 @@ public:
         return(getMax(rootPtr->right));
     }
     
-    Node* insert(Node* root, string& newVal)
+    string getMax2() const
+    {
+        Node* current = root;
+        
+        if (current == nullptr)
+            return("null");
+        
+        while (current->right != nullptr)
+            current = current->right;
+        return(current->value);
+    }
+    
+    void insert(string& newVal)
+    {
+        if (root == nullptr)
+            root = new Node(newVal);
+        
+        Node* current = root;
+        
+        if (newVal == current->value)
+            return;
+        
+        for(;;)
+        {
+            if (newVal < current->value)
+            {
+                if (current->left != nullptr)
+                    current = current->left;
+                else
+                {
+                    current->left = new Node(newVal);
+                    return;
+                }
+            }
+            else if (newVal > current->value)
+            {
+                if (current->right != nullptr)
+                    current = current->right;
+                else
+                {
+                    current->right = new Node(newVal);
+                    return;
+                }
+            }
+        }
+    }
+    
+    Node* recursiveInsert(Node* root, string& newVal)
     {
         if (this->root == nullptr)
         {
@@ -70,18 +131,29 @@ public:
         if (newVal < root->value)
         {
             if (root->left != nullptr)
-                insert(root->left, newVal);
+                recursiveInsert(root->left, newVal);
             else
                 root->left = new Node(newVal);
         }
         else if (newVal > root->value)
         {
             if (root->right != nullptr)
-                insert(root->right, newVal);
+                recursiveInsert(root->right, newVal);
             else
                 root->right = new Node(newVal);
         }
         return(root);
+    }
+    
+    void freeTree(Node* current)
+    {
+        if (current == nullptr)
+            return;
+        
+        freeTree(current->left);
+        freeTree(current->right);
+        free(current);
+        root = nullptr;
     }
     
 private:
@@ -98,15 +170,16 @@ int main() {
     string fourthVal = "bananas";
     string fifthVal = "strawberry";
     
-    root = bsTree.insert(root, firstVal);
-    bsTree.insert(root, secondVal);
-    bsTree.insert(root, thirdVal);
-    bsTree.insert(root, fourthVal);
-    bsTree.insert(root, fifthVal);
+    root = bsTree.recursiveInsert(root, firstVal);
+    bsTree.recursiveInsert(root, thirdVal);
+    bsTree.recursiveInsert(root, fourthVal);
+    bsTree.recursiveInsert(root, fifthVal);
 
+    bsTree.freeTree(root);
     cout << bsTree.getRootVal() << endl;
-    cout << bsTree.getMin(root) << endl;
-    cout << bsTree.getMax(root) << endl;
+    //cout << bsTree.getRootVal() << endl;
+    //cout << bsTree.getMin(root) << endl;
+    //cout << bsTree.getMax(root) << endl;
     
     
     
