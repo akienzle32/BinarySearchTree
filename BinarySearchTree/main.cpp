47ea7475fa9d129fb291965b7fd0d0775ba67c6e
Alec Kienzle <alec.kienzle@gmail.com>
2021-08-30 12:51:07 -0700

    added BinarySearchTree class

--- BinarySearchTree/main.cpp
+++ BinarySearchTree/main.cpp
@@ -19,6 +19,54 @@ struct Node
     Node* right;
 };
 
+class BinarySearchTree
+{
+public:
+    BinarySearchTree() : root(nullptr)
+    {}
+    
+    void insert(string& newVal)
+    {
+        if (root == nullptr)
+        {
+            root = new Node(newVal);
+            return;
+        }
+        Node *current = root;
+        
+        for(;;)
+        {
+            if (newVal == current->value)
+                return;
+        
+            if (newVal < current->value)
+            {
+                if (current->left != nullptr)
+                    current = current->left; // Think this is where you'd use recursion.
+                else
+                {
+                    current->left = new Node(newVal);
+                    return;
+                }
+            }
+            else if (newVal > current->value)
+            {
+                if (current->right != nullptr)
+                    current = current->right;
+                else
+                {
+                    current->right = new Node(newVal);
+                    return;
+                }
+            }
+        }
+        
+    }
+    
+private:
+    Node* root;
+};
+
 int main() {
     
     return(0);
